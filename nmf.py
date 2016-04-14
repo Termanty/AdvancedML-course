b7fb552c17dbf9dc03687c501369d8db270ad3db
termanty <termanty@gmail.com>
2016-04-14 07:11:45 +0300

    nmf: image plotting practice

--- nmf.py
+++ nmf.py
@@ -7,14 +7,16 @@
 # python 3
 
 import numpy as np
+import scipy
 import urllib
 from matplotlib import pyplot as plt
 
 
 url = 'https://cs.helsinki.fi/u/aklami/teaching/AML/exercise_4.csv'
-data = np.loadtxt(urllib.request.urlopen(url), delimiter=',')
-# data = np.loadtxt('exercise_4.csv', delimiter=',')
+# data = np.loadtxt(urllib.request.urlopen(url), delimiter=',')
+data = np.loadtxt('exercise_4.csv', delimiter=',')
 
 print(data.shape)
-
-
+# scipy.misc.imsave('outfile.jpg', data[0].reshape(16,16))
+plt.imshow(data[0].reshape(16,16), cmap='Greys_r')
+plt.show()
